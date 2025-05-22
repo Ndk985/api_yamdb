c3361376ecf89c087dd6a3ed71f6ef0fba1d8f21
Ndk985 <118891786+Ndk985@users.noreply.github.com>
2025-05-22 12:53:57 +0300

    фикс импортов

--- api_yamdb/api/reviews/views.py
+++ api_yamdb/api/reviews/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.permissions import (IsAuthenticatedOrReadOnly)
-from rest_framework.response import Response
-from reviews.models import Title, Review, Comment
+from reviews.models import Title, Review
 from api.core.permissions import IsAuthorOrModeratorOrAdmin
 from .serializers import (
     ReviewSerializer, CommentSerializer
